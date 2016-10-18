16c3055f01529bf76550863b471761711a2ecf2a
Jakub Wida <Jakub Wida>
2016-10-18 09:32:59 +0200

    added z dimension

--- Projekt/pygame_iso_2.py
+++ Projekt/pygame_iso_2.py
@@ -24,6 +24,15 @@ class Map_Block():
 	def __init__(self):
 		self.image = Block_Img
 Map_of_blocks =\
+[[
+[Map_Block(),Map_Block(),Map_Block(),Map_Block(),Map_Block(),Map_Block()],
+[Map_Block(),Map_Block(),Map_Block(),Map_Block(),Map_Block(),Map_Block()],
+[Map_Block(),Map_Block(),Map_Block(),Map_Block(),Map_Block(),Map_Block()],
+[Map_Block(),Map_Block(),Map_Block(),Map_Block(),Map_Block(),Map_Block()],
+[Map_Block(),Map_Block(),Map_Block(),Map_Block(),Map_Block(),Map_Block()],
+[Map_Block(),Map_Block(),Map_Block(),Map_Block(),Map_Block(),Map_Block()],
+[Map_Block(),Map_Block(),Map_Block(),Map_Block(),Map_Block(),Map_Block()]
+],			
 [
 [Map_Block(),Map_Block(),Map_Block(),Map_Block(),Map_Block(),Map_Block()],
 [Map_Block(),Map_Block(),Map_Block(),Map_Block(),Map_Block(),Map_Block()],
@@ -31,29 +40,42 @@ Map_of_blocks =\
 [Map_Block(),Map_Block(),Map_Block(),Map_Block(),Map_Block(),Map_Block()],
 [Map_Block(),Map_Block(),Map_Block(),Map_Block(),Map_Block(),Map_Block()],
 [Map_Block(),Map_Block(),Map_Block(),Map_Block(),Map_Block(),Map_Block()],
+[Map_Block(),Map_Block(),Map_Block(),Map_Block(),Map_Block(),Map_Block()]
+],
+[
 [Map_Block(),Map_Block(),Map_Block(),Map_Block(),Map_Block(),Map_Block()],
-]			
+[Map_Block(),Map_Block(),Map_Block(),Map_Block(),Map_Block(),Map_Block()],
+[Map_Block(),Map_Block(),Map_Block(),Map_Block(),Map_Block(),Map_Block()],
+[Map_Block(),Map_Block(),Map_Block(),Map_Block(),Map_Block(),Map_Block()],
+[Map_Block(),Map_Block(),Map_Block(),Map_Block(),Map_Block(),Map_Block()],
+[Map_Block(),Map_Block(),Map_Block(),Map_Block(),Map_Block(),Map_Block()],
+[Map_Block(),Map_Block(),Map_Block(),Map_Block(),Map_Block(),Map_Block()]
+]]			
+			
 
-Map_of_blocks[1][1].image=Ant_Img
+Map_of_blocks[0][1][1].image=Ant_Img
 
 block_x_size=16
 block_y_size=16
 
-def carthesian_to_iso(xy):
-	iso_x=(xy[0]-xy[1])/2.0
-	iso_y=(xy[0]+xy[1])/2.0
+def carthesian_to_iso(xyz):
+	iso_x=(xyz[0]-xyz[1])/2.0
+	iso_y=(xyz[0]+xyz[1]-xyz[2])/2.0
 	return [iso_x,iso_y]
 
-def draw_blocks(map_of_blocks,x_draw_size,y_draw_size,map_x_offset,map_y_offset,draw_zero_x,draw_zero_y):
+def draw_blocks(map_of_blocks,x_draw_size,y_draw_size,z_draw_size,map_x_offset,map_y_offset,map_z_offset,draw_zero_x,draw_zero_y):
 	x_len=len(map_of_blocks)
 	y_len=len(map_of_blocks[0])
-
-	for x in range(map_x_offset,x_draw_size):
-		for y in range(map_y_offset,y_draw_size):
-			iso_pos=carthesian_to_iso([x*16,y*16])
-			DISPLAYSURF.blit(map_of_blocks[x][y].image,(iso_pos[0]+draw_zero_x,iso_pos[1]+draw_zero_y))
+	for z in range(map_z_offset,z_draw_size):
+		for x in range(map_x_offset,x_draw_size):
+			for y in range(map_y_offset,y_draw_size):
+				iso_pos=carthesian_to_iso([x*16,y*16,z*16])
+				DISPLAYSURF.blit(map_of_blocks[z][x][y].image,(iso_pos[0]+draw_zero_x,iso_pos[1]+draw_zero_y))
 		
-screen_x_center=DISPLAYSURF.get_width()/2				
+screen_x_center=DISPLAYSURF.get_width()/2	
+screen_y_center=DISPLAYSURF.get_height()/2
+
+			
 		
 #pygame.draw.rect(DISPLAYSURF, BLACK, (200, 150, 100, 50))
 while True:
@@ -65,5 +87,5 @@ while True:
 	
 	
 	
-	draw_blocks(Map_of_blocks,3,3,0,0,screen_x_center,100)
+	draw_blocks(Map_of_blocks,3,3,3,0,0,0,screen_x_center,0)
 	pygame.display.update()
