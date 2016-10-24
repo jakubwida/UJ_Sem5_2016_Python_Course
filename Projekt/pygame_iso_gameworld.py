827981b834e95a9e7943b047100ceda95a146161
Jakub Wida <Jakub Wida>
2016-10-24 08:51:34 +0200

    minor improvements

--- Projekt/pygame_iso_gameworld.py
+++ Projekt/pygame_iso_gameworld.py
@@ -10,10 +10,12 @@ Ant_Img=pygame.image.load("Ant_Example.png")
 #niedkonczone ponizej
 class Abstract_Block:
 	#self.container_key="Abstract_Block"
-	def __init__(self,xyz_coords,game_map):
+	def __init__(self,xyz_coords,game_map,block_list_container):
 		self.image=Block_Img
 		self.xyz=xyz_coords
 		self.Game_Map=game_map
+		self.Block_List_Container=block_list_container
+		
 		#trzeba dodac: 
 		#private_container, ktory bedzie wsadzal bloczka do 
 		#listy po utworzniu, i usuwal po zniszczeniu, 
@@ -24,8 +26,17 @@ class Abstract_Block:
 class Block_List_Container:
 	def __init__(self):
 		self.dictionary={}
-#	def add_to_container()
+	def add_to_container(Block):
+		if(Block.container_key in self.dictionary):
+			self.dictionary.get(Block.container_key).append(Block)
+		else:
+			self.dictionary[Block.container_key]=[Block]
+	def remove_from_container(Block):
+		list = self.dictionary.get(Block.container_key)
+		list.remove(Block)
 
+	def get_list_of_blocks_by_container_key(key):
+		return self.dictionary.get(key)
 
 
 
@@ -48,11 +59,13 @@ class Grass_Block:
 
 #koniec niedkokonczenia
 
-class Empty_Block:
-	def __init__(self):
+class Empty_Block(Abstract_Block):
+	def __init__(self,xyz_coords,game_map,block_container):
+		Abstract_Block.__init__(self,xyz_coords,game_map,block_container)
 		self.image=None
-class Rock_Block:
-	def __init__(self):
+class Rock_Block(Abstract_Block):
+	def __init__(self,xyz_coords,game_map,block_container):
+		Abstract_Block.__init__(self,xyz_coords,game_map,block_container)
 		self.image=Rock_Img
 
 
@@ -63,14 +76,19 @@ def random_bool(percent=50):
 class Game_Map:
 	def __init__(self):
 		print("Game_Map created")
+		self.block_container=Block_List_Container()
 	def construct_world(self,xyz_size):
-		self.world=[[[ self.random_block()  for z in range(xyz_size[2])] for  x in range(xyz_size[0])] for y in range(xyz_size[1])]
+		self.world=[[[ self.random_block([x,y,z])  for z in range(xyz_size[2])] for  x in range(xyz_size[0])] for y in range(xyz_size[1])]
 		
-
-	def random_block(self):
+	def get_block_at_xyz(x,y,z):
+		return self.world[x][y][z]
+	def set_block_at_xyz(x,y,z,Block):
+		self.world[x][y][z]=Block
+	
+	def random_block(self,xyz_coords):
 		coin=random_bool(50)
 		if coin:
-			return Rock_Block()
+			return Rock_Block(xyz_coords,self,self.block_container)
 		else:
-			return Empty_Block()
+			return Empty_Block(xyz_coords,self,self.block_container)
 			
